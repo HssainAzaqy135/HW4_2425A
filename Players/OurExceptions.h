3e8c7ad6c787c808c7a8b29ef7a07550d934e2d1
HssainAzaqy135 <kinghssain135@gmail.com>
2025-01-28 13:29:31 +0200

    update UML for exception classes, removed reference from exceptions constructors

--- Players/OurExceptions.h
+++ Players/OurExceptions.h
@@ -12,15 +12,15 @@ const string CANT_PAY_COINS = "Can't Pay Coins";
 
 class InvalidPlayersFile : public runtime_error{
 public:
-    InvalidPlayersFile(const string& what = INVALID_PLAYERS_FILE) : runtime_error(what) {};
+    InvalidPlayersFile(const string what = INVALID_PLAYERS_FILE) : runtime_error(what) {};
 };
 
 class InvalidEventsFile : public runtime_error {
 public:
-    InvalidEventsFile(const string& what = INVALID_EVENTS_FILE) : runtime_error(what) {};
+    InvalidEventsFile(const string what = INVALID_EVENTS_FILE) : runtime_error(what) {};
 };
 
 class CantPayCoins : public runtime_error {
 public:
-    CantPayCoins(const string& what = CANT_PAY_COINS) : runtime_error(what) {};
+    CantPayCoins(const string what = CANT_PAY_COINS) : runtime_error(what) {};
 };
